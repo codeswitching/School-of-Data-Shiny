71f73b69c3703822de806de974ef680506a22dc2
Lauren Steely <lwsteely@gmail.com>
2019-02-01 15:04:40 -0800

    changes to example 1

--- example1/app.R
+++ example1/app.R
@@ -1,16 +1,26 @@
+# Example 1 - Urban water use - Non-interactive version
+
 library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Urban water conservation report"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
+        selectInput("region",
+                  label = "Choose a region:",
+                  choices = list("Colorado River" = "Colorado River", "Central Coast" = "Central Coast",
+                                 "North Coast" = "North Coast", "North Lahontan" = "North Lahontan",
+                                 "South Lahontan" = "South Lahontan", "South Coast" = "South Coast",
+                                 "San Francisco Bay" = "San Francisco Bay",
+                                 "San Joaquin River" = "San Joaquin River",
+                                 "Sacramento River" = "Sacramento River", "Tulare Lake" = "Tulare Lake")),
          sliderInput("bins",
-                     "Number of bins:",
+                     label = "Choose the number of bins:",
                      min = 1,
                      max = 50,
                      value = 30)
@@ -18,21 +28,29 @@ ui <- fluidPage(
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         plotOutput("histogramPlot")
       )
    )
 )
 
-# Define server logic required to draw a histogram
+################################ Define server logic required to draw a histogram
+
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  
+  library(tidyverse)
+  
+  # Read in urban water agency conservation data by month for 2014-2019
+  waterdata <- read_csv("CA urban water conservation 2019-01.csv",
+                        col_types = cols(month = col_date(format = "%m/%d/%Y")))
+  
+  output$histogramPlot <- renderPlot({
+    # create a filtered subset of the data based on the selected input region
+    waterdata_filter <- filter(waterdata, region == input$region)
+     # Now we'll make a new histogram just for that region
+     hist(waterdata_filter$gpcd,
+          breaks = input$bins,
+          xlab = "gallons per person per day",
+          main = paste0("Urban residential water use for ", input$region, ", 2014-2018"))
    })
 }
 
--- example1/example1.R
+++ example1/example1.R
@@ -1,11 +1,27 @@
-# Example 1
+# Example 1 - Urban water use - Non-interactive version
 
-x <- faithful$waiting
+library(tidyverse)
 
-numbins <- 22
+# Read in urban water agency conservation data by month for 2014-2019
+waterdata <- read_csv("example1/CA urban water conservation 2019-01.csv",
+                      col_types = cols(month = col_date(format = "%m/%d/%Y")))
 
-bins <- seq(min(x), max(x), length.out = numbins + 1)
+summary(waterdata)  # get summary statistics on the numeric variables
+unique(waterdata$region)  # show all unique regions
 
-hist(x, breaks = bins, col = "#75AADB", border = "white",
-     xlab = "Waiting time to next eruption (in mins)",
-     main = "Histogram of waiting times")
+# GPCD = gallons per capita (person) per day, a common metric for urban water use efficiency
+hist(waterdata$gpcd)  # examine the range of gpcds
+
+# Now let's customize the histogram a bit
+
+selectedregion <- "South Coast"     # select a region
+numbins <- 8                           # choose a number of bins for the histogram
+waterdata_filter <- waterdata %>%   # subset waterdata by the selected region 
+  filter(region == selectedregion)
+
+# Now we'll make a new histogram just for that region
+
+hist(waterdata_filter$gpcd,
+     breaks = numbins,
+     xlab = "gallons per person per day",
+     main = paste0("Urban residential water use for ", selectedregion, ", 2014-2018"))
